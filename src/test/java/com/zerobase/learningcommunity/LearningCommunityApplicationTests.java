5fb1f72a26cff13c1d6fa2b86c6714e9aeeeafab
Sim Juhyun <154858191+JuhyunSim@users.noreply.github.com>
2024-06-19 15:22:07 +0900

    first commit : 프로젝트 연결 및 README.md 작성

---
+++ src/test/java/com/zerobase/learningcommunity/LearningCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.learningcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
